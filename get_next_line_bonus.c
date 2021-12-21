4eb7c1639fd1234fd91e2f9cee106db793390a98
Naima Sar <nsar@c1r11s1.42lausanne.ch>
2021-12-21 14:23:12 +0100

    first commit

---
+++ get_next_line_bonus.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_char.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsar <marvin@42lausanne.ch>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 13:43:55 by nsar              #+#    #+#             */
+/*   Updated: 2021/12/08 15:49:14 by nsar             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_read_and_save(int fd, char *save)
+{
+	char	*buff;
+	int		ret;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	ret = 1;
+	while (!ft_strchr(save, '\n') && ret != 0)
+	{
+		ret = read(fd, buff, BUFFER_SIZE);
+		if (ret == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[ret] = '\0';
+		save = ft_strjoin(save, buff);
+	}
+	free(buff);
+	return (save);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;	
+	static char	*save;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	save = ft_read_and_save(fd, save);
+	if (!save)
+		return (NULL);
+	line = ft_select(save);
+	save = ft_select2(save);
+	return (line);
+}
